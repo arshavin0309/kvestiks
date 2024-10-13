6b2641c6255e4730a3f28112f658432a3f4c6260
Иванов Владислав <arshavin0309@mail.ru>
2024-10-13 21:48:43 +0500

    add

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,26 @@
 const swiper = new Swiper('.swiper1', {
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 30,
+    breakpoints: {
+        320: {
+            slidesPerView: 1.13,
+            spaceBetween: 20
+        },
+        500: {
+            slidesPerView: 1.5,
+            spaceBetween: 25
+        },
+        900: {
+            slidesPerView: 2.5,
+            spaceBetween: 25
+        },
+        900: {
+            slidesPerView: 2,
+            spaceBetween: 25
+        },
+        1350: {
+            slidesPerView: 3,
+            spaceBetween: 25
+        }
+    },
 
     pagination: {
         el: '.swiper-pagination',
